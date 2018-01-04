6a90b2119cc5c617e11f99fa9a5075f90b5c9216
TokAhmet <ahmet.ordulu@hotmail.se>
2018-01-04 01:06:46 +0100

    uppdrag sida + lite tweaks

--- script/script.js
+++ script/script.js
@@ -1,7 +1,13 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mySidenav").style.width = "50%";
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+}
+
+function framtiden(){
+
+		window.location.href = "http://framtiden.se";
+
 }
